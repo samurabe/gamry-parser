8f768e38c24ac7b19cec11200c9beb236a46ff06
samurabe~ <117753984+samurabe@users.noreply.github.com>
2022-11-15 15:07:33 +0100

    Revert "Update eispot.py"
    This reverts commit 4effec6b098b879c4a3acbe37c1fa7ea89554305.

--- gamry_parser/eispot.py
+++ gamry_parser/eispot.py
@@ -12,7 +12,6 @@ class Impedance(parser.GamryParser):
 
         Returns:
             pandas.DataFrame:
-                - T: time, in seconds or Timestamp
                 - Freq (float): frequency, in Hz
                 - Zreal (float): Real Impedance, in ohms
                 - Zimag (float): Imaginary Impedance, in ohms
@@ -23,4 +22,4 @@ class Impedance(parser.GamryParser):
 
         assert self.loaded, "DTA file not loaded. Run Impedance.load()"
         df = self._curves[curve]
-        return df[["T","Freq", "Zreal", "Zimag", "Zmod", "Zphz"]]
+        return df[["Freq", "Zreal", "Zimag", "Zmod", "Zphz"]]
